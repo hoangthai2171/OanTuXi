60ba56884dc9811a09da246b11c6aaf854b92d6a
hoangthai2171 <hoangthai2171@gmail.com>
2018-03-20 18:16:31 +0700

    Oan Tu Xi

---
+++ OanTuXi/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace OanTuXi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ipserver = new IPEndPoint(IPAddress.Any, 995);
+            server.Bind(ipserver);
+            EndPoint remote = ipserver;
+            
+            
+
+            
+            while (true)
+            {
+                byte[] receiveData = new byte[20];
+                server.ReceiveFrom(receiveData, ref remote);
+                int clientResult = Convert.ToInt32(Encoding.ASCII.GetString(receiveData));
+                Random rd = new Random();
+                int serverResult = rd.Next(0, 3);
+                switch (serverResult)
+                {
+                    case 0:
+                        Console.WriteLine("Server chon: Keo");
+                        break;
+                    case 1:
+                        Console.WriteLine("Server chon: Bua");
+                        break;
+                    case 2:
+                        Console.WriteLine("Server chon: Bao");
+                        break;
+
+                }
+                
+                    if ((serverResult == 0 && clientResult == 0) || (serverResult == 1 && clientResult == 1) || (serverResult == 2 && clientResult == 2))
+                    {
+                        byte[] sendData = Encoding.ASCII.GetBytes("Draw");
+                        
+                        server.SendTo(sendData, remote);
+                    }
+                    if ((serverResult == 0 && clientResult == 2) || (serverResult == 1 && clientResult == 0) || (serverResult == 2 && clientResult == 1))
+                    {
+                        byte[] sendData = Encoding.ASCII.GetBytes("Lose");
+                        server.SendTo(sendData, remote);
+                    }
+                    if ((serverResult == 2 && clientResult == 0) || (serverResult == 0 && clientResult == 1) || (serverResult == 1 && clientResult == 2))
+                    {
+                        byte[] sendData = Encoding.ASCII.GetBytes("Win");
+                        server.SendTo(sendData, remote);
+                    }
+                }
+            }
+        }
+    }
+
---
+++ OanTuXi_Client/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace OanTuXi_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 995);
+            EndPoint remote = (EndPoint)ipe;
+
+            for (int i = 0; i < 100000; i++)
+            {
+                Console.WriteLine("Moi ban chon KEO(0) BUA(1) BAO(2), vui long nhap so theo thu tu: ");
+                int S = int.Parse(Console.ReadLine());
+                
+                if (S == 0)
+                {
+
+                    Console.WriteLine("Ban chon 'Keo'.");
+                    byte[] sendData = Encoding.ASCII.GetBytes("0");
+
+                    server.SendTo(sendData, remote);
+                    byte[] receiveData = new byte[20];
+                    server.ReceiveFrom(receiveData, ref remote);
+                    Console.WriteLine("Ket qua la: {0}", Encoding.ASCII.GetString(receiveData));
+                }
+                if (S == 1)
+                {
+
+                    Console.WriteLine("Ban chon 'Bua'.");
+                    byte[] sendData = Encoding.ASCII.GetBytes("1");
+
+                    server.SendTo(sendData, remote);
+                    byte[] receiveData = new byte[20];
+                    server.ReceiveFrom(receiveData, ref remote);
+                    Console.WriteLine("Ket qua la: {0}", Encoding.ASCII.GetString(receiveData));
+                }
+                if (S==2)
+                {
+
+                    Console.WriteLine("Ban chon 'Bao'.");
+                    byte[] sendData = Encoding.ASCII.GetBytes("2");
+
+                    server.SendTo(sendData, remote);
+                    byte[] receiveData = new byte[20];
+                    server.ReceiveFrom(receiveData, ref remote);
+                    Console.WriteLine("Ket qua la: {0}", Encoding.ASCII.GetString(receiveData));
+                }
+            }
+        }
+    }
+}
